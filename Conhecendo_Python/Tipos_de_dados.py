a6e428e795daa07860a6e6c7a91187f030c5fed4
lorranadocarmo <lorranadocarmo.dev@gmail.com>
2024-08-22 21:22:29 -0300

    Create Tipos_de_dados.py

---
+++ Conhecendo_Python/Tipos_de_dados.py
@@ -0,0 +1,14 @@
+print(11+10+1515165)
+print (1.5+1+2.5)
+print(True)
+print(False)
+print("Python")
+
+int()
+#numeros inteiros
+float()
+#numeros de ponto flutuante
+str()
+#string
+bool()
+#True ou False
